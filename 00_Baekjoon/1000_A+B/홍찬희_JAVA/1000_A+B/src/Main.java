fd9ac6eaa69006fefc348f80c794ca99ffb16df7
ghdcksgml2 <88768832+ghdcksgml2@users.noreply.github.com>
2021-08-15 16:17:42 +0900

    Add files via upload

---
+++ 00_Baekjoon/1000_A+B/홍찬희_JAVA/1000_A+B/src/Main.java
@@ -0,0 +1,19 @@
+// 언어 : JAVA , (성공/실패) : 1/1 , 메모리 : 14200 KB , 시간 : 140ms
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A+B);
+    }
+}
