febb48afd73dfddd701958ffcf87ebf1d5cf6f06
jusung-c <62228433+jusung-c@users.noreply.github.com>
2021-08-16 17:53:16 +0900

    Add files via upload

---
+++ 00_Baekjoon/2557_Hello World/이주성_JAVA/2557_Hello World.java
@@ -0,0 +1,9 @@
+// 언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 13888 KB , 시간 : 124ms
+
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.print("Hello World!");
+
+    }
+}
