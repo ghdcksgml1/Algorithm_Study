2809b75c343f47b76b6aefd53627edff4832c6e9
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2021-08-19 18:13:26 +0900

    Add files via upload

---
+++ 00_Baekjoon/1008_A:B/소경민_JAVA/1008_A%B/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        Scanner a = new Scanner(System.in);
+        
+        double A = a.nextDouble();
+        double B = a.nextDouble();
+        
+        System.out.println(A/B);
+        a.close();
+
+	}
+
+}
