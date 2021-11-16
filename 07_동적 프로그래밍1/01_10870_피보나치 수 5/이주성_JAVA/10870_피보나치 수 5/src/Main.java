74be3d3ace1be5589da4400229285a9bbd118dd9
jusung-c <62228433+jusung-c@users.noreply.github.com>
2021-11-17 00:31:48 +0900

    Update Main.java

--- 07_동적 프로그래밍1/01_10870_피보나치 수 5/이주성_JAVA/10870_피보나치 수 5/src/Main.java
+++ 07_동적 프로그래밍1/01_10870_피보나치 수 5/이주성_JAVA/10870_피보나치 수 5/src/Main.java
@@ -1,24 +1,24 @@
-// 언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 15804 KB , 시간 : 140 ms
+// 언어 : JAVA , (성공/실패) : 2/3 , 메모리 : 15848 KB , 시간 : 144 ms
 
 import java.io.*;
 
 public class Main {
-    public static int fib(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1){
-            return 1;
-        } else {
-            return fib(n-1) + fib(n-2);
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
+        int[] fib = new int[n+1];
 
-        bw.write(fib(n)+" ");
+        for(int i=0; i<=n; i++) {
+            if (i == 0) fib[i] = 0;
+            else if (i == 1) fib[i] = 1;
+            else {
+                fib[i] = fib[i-1] + fib[i-2];
+            }
+        }
+
+        bw.write(fib[n]+" ");
 
         bw.close();
     }
