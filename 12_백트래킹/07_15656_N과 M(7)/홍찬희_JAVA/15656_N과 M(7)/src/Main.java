2633e1a77de77f5c75b5f728d0a159610eee1848
홍찬희 <ghdcksgml2@naver.com>
2022-01-11 11:41:25 +0900

    찬희) 15656_N과 M(7)

---
+++ 12_백트래킹/07_15656_N과 M(7)/홍찬희_JAVA/15656_N과 M(7)/src/Main.java
@@ -0,0 +1,43 @@
+// 언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 138380 KB , 시간 : 660 ms
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static int N,M;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void backtracking(int x,String num,int prev) throws IOException {
+        if(x == M){
+            bw.write(num+"\n");
+            return;
+        }
+        for(int i=0;i<N;i++){
+//            if(visit[i]) continue;
+            visit[i] = true;
+            backtracking(x+1,num + (arr[i]+" "),i);
+            visit[i] = false;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visit = new boolean[N];
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        backtracking(0,"",0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
