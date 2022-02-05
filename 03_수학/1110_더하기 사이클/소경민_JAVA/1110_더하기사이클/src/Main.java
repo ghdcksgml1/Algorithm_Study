e06a0b04e6d169082d569260504ac0dbf7ab22b6
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2022-02-06 01:50:42 +0900

    Add files via upload

---
+++ 03_수학/1110_더하기 사이클/소경민_JAVA/1110_더하기사이클/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		
+		int N = a.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		a.close();
+
+	}
+
+}
