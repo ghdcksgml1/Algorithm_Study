ca70f99e5c7d6583ed68c7f947977b1248b85c2a
이주성 <62228433+jusung-c@users.noreply.github.com>
2022-02-23 17:35:10 +0900

    Update Main.java

--- 12_백트래킹/10_15664_N과 M(10)/이주성_JAVA/15664_N과 M (10)/src/Main.java
+++ 12_백트래킹/10_15664_N과 M(10)/이주성_JAVA/15664_N과 M (10)/src/Main.java
@@ -2,65 +2,60 @@
 // 메모리 : 27160 KB , 시간 : 352 ms
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N, M, sum;
-    static int[] selected, num, visit;
-    static LinkedHashSet<String> result;
+    static int N, M;
+    static int[] nums, selected;
+    static LinkedHashSet<String> LHset = new LinkedHashSet<>();
 
     public static void main(String[] args) throws IOException {
+        input();
+        
+        String numString = "";
+        Arrays.sort(nums);
+
+        rec_func(1, 0, numString);
+
+        Iterator<String> iter = LHset.iterator();
+        while (iter.hasNext()) {
+            bw.write(iter.next()+"\n");
+        }
+
+        bw.close();
+    }
+
+    static void rec_func(int k, int prev, String numString) throws IOException {
+        if (k == M + 1) {
+            LHset.add(numString);
+        } else {
+            for (int i = prev + 1; i <= N; i++) {
+                int n = nums[i];
+
+                selected[k] = n;
+                rec_func(k + 1, i, numString + n + " ");
+                selected[k] = 0;
+            }
+        }
+    }
+
+    static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        nums = new int[N + 1];
         selected = new int[M + 1];
-        num = new int[N + 1];
-        visit = new int[N + 1];
-        result = new LinkedHashSet<String>();
-        String numString = "";
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(num);
-
-        rec_func(1, 0, numString);
-
-        Iterator<String> iterator = result.iterator();
-        while (iterator.hasNext()) {
-            bw.write(iterator.next());
-            bw.write("\n");
-        }
-
-        bw.close();
-    }
-
-    private static void rec_func(int k, int prev, String numString) throws IOException {
-        if (k == M + 1) {
-            for (int i = 1; i <= M; i++) {
-                result.add(numString);
-            }
-
-        } else {
-            for (int i = prev + 1; i <= N; i++) {
-                int n = num[i];
-
-                if (visit[i] == 1) continue;
-
-                selected[k] = n;
-                visit[i] = 1;
-
-                rec_func(k + 1, i, numString + n + " ");
-
-                selected[k] = 0;
-                visit[i] = 0;
-            }
+            nums[i] = Integer.parseInt(st.nextToken());
         }
     }
 }
