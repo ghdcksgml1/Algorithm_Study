53149d4cc091a82872fcb7f248f4fa82ad93bced
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-03-15 10:08:59 +0900

    Add files via upload

---
+++ 07_동적 프로그래밍1/04_2xn 타일링/정유빈_Python/11726_2xn_타일링.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	int dp[1001] = { 0 }; //배열 생성
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) %10007;
+	}
+
+	cout << dp[n];
+
+
+}
