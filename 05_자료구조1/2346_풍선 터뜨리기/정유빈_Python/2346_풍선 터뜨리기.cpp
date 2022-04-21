d0aa1519ce32ffea3da3cefdad27f0bc5d2e4abb
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-04-22 03:27:23 +0900

    Add files via upload

--- 05_자료구조1/2346_풍선 터뜨리기/정유빈_Python/2346_풍선 터뜨리기.cpp
+++ 05_자료구조1/2346_풍선 터뜨리기/정유빈_Python/2346_풍선 터뜨리기.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
-#include <utility>
 #include <deque>
-
+#include <utility>
 using namespace std;
 
-deque <pair<int, int>> dq; //두개의 정수 값을 하나로 묶음
+
 int main() {
-    ios_base::sync_with_stdio(0); //입출력 시간 줄이기
-    cin.tie(0); 
-    cout.tie(0);
+	ios_base::sync_with_stdio(0); //입출력 빠르게
+	cin.tie(0);
+	cout.tie(0);
 
-    int N;
-    cin >> N;
+	int n; //풍선 개수
+	cin >> n;
 
-    for (int i = 0; i < N; i++) {
-        int tmp;
-        cin >> tmp;
-        dq.push_back(make_pair(tmp, i + 1)); //인덱스와 값을 하나로 묶어서 저장
-    }
+	deque <pair<int, int> > dq(n);
 
-    while (true) {
-        int cnt = dq.front().first;
-        cout << dq.front().second << " ";
-        dq.pop_front(); //첫번째 원소값 삭제
+	for (int i = 0; i < n; i++) {
+		dq[i].first = i + 1;
+		cin >>  dq[i].second;
+	}
 
-        if (dq.empty()) { //덱이 비어있으면 종료
-            break;
-        }
-        else {
-            if (cnt > 0) {
-                for (int i = 0; i < cnt - 1; i++) {
-                    dq.push_back(dq.front()); //제일 앞의 원소를 복제하여 뒤로 전달
-                    dq.pop_front(); //제일 앞의 원소 삭제
-                }
-            }
-            else {
-                for (int i = 0; i < abs(cnt); i++) {
-                    dq.push_front(dq.back()); //제일 뒤의 원소를 복제하여 앞으로 전달
-                    dq.pop_back(); //제일 뒤의 원소 삭제
-                }
-            }
-        }
-    }
+	int m = dq.front().second;
+	cout << dq.front().first << " ";
+	dq.pop_front();
 
+	while (dq.size() > 0) {
+
+		if (m > 0) { 
+			for (int i = 1; i < m; i++) {
+				dq.push_back(make_pair(dq.front().first, dq.front().second));
+				dq.pop_front();
+
+			}
+		}
+		else {
+			m = abs(m); //3
+			for (int i = 0; i < m; i++) {
+				dq.push_front(make_pair(dq.back().first, dq.back().second));
+				dq.pop_back();
+			}
+		}
+
+		cout << dq.front().first << " "; //4
+		m = dq.front().second; //-3
+		dq.pop_front();
+		
+	}
+	
 }
